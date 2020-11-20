ef3ecb91e74e5b57e97e118b1bdef413590947c1
bluesmli <blues_mli@163.com>
2020-11-20 16:54:29 +0800

    冒泡排序

---
+++ algt/src/main/java/algorithms/sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package algorithms.sort;
+
+public class BubbleSort {
+
+    /*冒泡排序*/
+
+    public  static  int [] ArrayBubbleSort(int [] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    //交换
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int [] nums=ArrayBubbleSort(new int[]{10,5,6,9,4});
+        for (int i:nums
+             ) {
+            System.out.println(i);
+        }
+    }
+
+
+}
